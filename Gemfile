81b632786128aa747a0dd55435c758e816117621
Jacqueline Chenault <adigitalnative@gmail.com>
2018-12-21 15:20:20 -0500

    Version Bump & rework for new app

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in thespis.gemspec
 gemspec
+gem 'byebug'
--- lib/thespis/runner.rb
+++ lib/thespis/runner.rb
@@ -3,7 +3,7 @@ require 'uri'
 require 'open-uri'
 require 'nokogiri'
 require 'cgi'
-
+require 'byebug'
 
 module Thespis
   class Runner
@@ -11,26 +11,74 @@ module Thespis
       "Hello"
     end
 
-    def self.pull_playbill(states)
-      notices = []
-      states = parse_states(states)
-      states.each do |state|
-        uri = URI.parse("http://www.playbill.com/job/listing")
-        uri.query = "q=&category=Performer&date=&state=#{state}&paid=on"
-        body = Net::HTTP.get_response(uri).body
-        html_doc = Nokogiri::HTML(body)
-        html_doc.css("table.bsp-table tr").each do |listing|
-          title = "Playbill - #{listing.children[1].css("a span").text}"
-          listing_url = listing.children[1].css("a")[0]['href']
-          link = "http://www.playbill.com#{listing_url}"
-          details = pull_details(listing_url)
-          notices << { title: title, link: link, details: details }
-        end
+    def self.search(states)
+      parsed_states = parse_states(states)
+
+      listings = parsed_states.map do |state|
+        fetch_playbill(state)
       end
-      notices
+
+      listings.flatten
+
+      # Could do deeper searching, but the data isn't regular in how it's divided on the page.
+      listings.flatten.map do |listing|
+
+        # Request listing-specific data
+        uri = URI.parse(listing[:url])
+        response_body = Net::HTTP.get_response(uri).body
+        html_doc = Nokogiri::HTML(response_body)
+
+        # Parse data for specific chunks
+        company = html_doc.css(".jobs-section").children[1].children[0].text.strip
+        audition_type = html_doc.css(".jobs-section")[1].children[4].children[1].text.strip
+        contract = html_doc.css(".jobs-section")[1].children[4].children[6].text.strip
+        # where = html_doc.css(".jobs-section")[2].children[4].text.strip # This might want additional parsing for address etc
+        # to_prepare = html_doc.css(".jobs-section")[2].children[6].text.strip
+
+        # Return object w/ all the chunks
+        {
+          source: listing[:source],
+          title: listing[:title],
+          company: company,
+          # where: where,
+          # to_prepare: to_prepare,
+          url: listing[:url],
+          state: listing[:state]
+        }
+      end
     end
 
-    private
+    def self.fetch_playbill(state)
+      uri = URI.parse("http://www.playbill.com/job/listing")
+      uri.query = "q=&category=Performer&date=&state=#{state}&paid=on"
+      body = Net::HTTP.get_response(uri).body
+      html_doc = Nokogiri::HTML(body)
+
+      parsed_listings = []
+
+      listings = html_doc.css("div.pb-tile-wrapper")
+
+      listings.each do |listing|
+
+        title = listing.children[1].css("div.pb-tile-title").children[0].to_s.strip
+        listing_url = listing.css('a')[0]['href']
+        url = "http://www.playbill.com#{listing_url}"
+
+        parsed_listings << {
+          source: "Playbill",
+          title: title,
+          url: url,
+          state: state
+        }
+      end
+
+      return parsed_listings
+    end
+
+    def self.fetch_listing(listing)
+      debugger
+    end
+
 
     def self.parse_states(states)
       if states.class == String
@@ -40,16 +88,6 @@ module Thespis
       else
         puts "States not valid"
       end
-    end
-
-    def self.pull_details(listing_url)
-      uri = URI.parse("http://www.playbill.com#{listing_url}")
-      body = Net::HTTP.get_response(uri).body
-      html_doc = Nokogiri::HTML(body)
-
-      company_info = html_doc.css("section.jobs-section")[0].text
-      details = html_doc.css("section.jobs-section").to_html
-      { company: company_info, details: details }
     end
   end
 end
